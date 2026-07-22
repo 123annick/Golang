a0c5c8ebed12628dcbd81cb8df6e3a24c8a1bb68
Annick Ujeneza <annickujeneza4@gmail.com>
2026-07-22 13:50:21 +0200

    Add Student struct in models package and wire it into main

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"go-learning-task/models"
+)
 
 func main() {
-	fmt.Println("Student Management System")
+	student := models.Student{
+		ID:     1,
+		Name:   "Annick",
+		Email:  "annick@example.com",
+		Course: "Software Engineering",
+		Score:  90,
+	}
+
+	fmt.Println(student)
 }
